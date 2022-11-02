d343fc7afb792963a3779a1282f017250a27246d
FZenhas <fpzenhas@gmail.com>
2022-11-02 09:37:41 +0000

    layout - 1st scratch and models

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+//use Illuminate\Database\Eloquent\Model;
 
 class User extends Authenticatable
 {
@@ -37,3 +38,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+
+/*class User extends Model
+{
+    public function usertype()
+    {
+        return $this->belongsTo('App\Usertype');
+    }
+}
+*/
---
+++ resources/views/master/header.blade.php
@@ -0,0 +1,44 @@
+<div id="viewport">
+    <!-- Sidebar -->
+    <div id="sidebar">
+        <header>
+
+        </header>
+        <ul class="nav flex-column" id="sidenav">
+            <li  data-toggle="collapse" data-target="#softskills" class="collapsed active">
+                <a class="nav-link" href="#">Análise de Soft Skills</a>
+            </li>
+            <ul class="sub-menu collapse" id="softskills">
+                <a class="nav-link" href="#">Criar testes</a>
+                <a class="nav-link" href="#">Registar notas</a>
+                <a class="nav-link" href="#">Análise Comparativa</a>
+            </ul>
+            <li  data-toggle="collapse" data-target="#managment" class="collapsed active">
+                <a class="nav-link" href="#">Gestão</a>
+            </li>
+            <ul class="sub-menu collapse" id="managment">
+                <a class="nav-link" href="#">Turmas</a>
+                <a class="nav-link" href="#">Alunos</a>
+                <a class="nav-link" href="#">Técnicos</a>
+            </ul>
+        </ul>
+    </div>
+    <!-- Content -->
+    <div id="content">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                    <li>
+                        <a href="#"></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container-fluid">
+            <h1>Soft Skills Analysis</h1>
+        </div>
+    </div>
+</div>
