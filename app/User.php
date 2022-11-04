a2de588057efca1e26e3748a771537e8e77b5cbc
Filomena Teles <111891232+FilomenaTeles@users.noreply.github.com>
2022-11-04 17:05:09 +0000

    edit users

--- app/User.php
+++ app/User.php
@@ -13,7 +13,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var array
      */
     protected $fillable = [
@@ -34,7 +33,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast to native types.
-     *
      * @var array
      */
     protected $casts = [
