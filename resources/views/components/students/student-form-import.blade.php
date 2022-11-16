0414758fcde6c3f58e75800d152f43ea81105b36
Maicon Mota <maiconlmotadev@hotmail.com>
2022-11-16 20:38:19 +0000

    Ajustes no import ficheiro

--- resources/views/components/students/student-form-import.blade.php
+++ resources/views/components/students/student-form-import.blade.php
@@ -15,36 +15,36 @@
         </table>
         <br>
 
-        <div class="container">
-            <p >Obervação 02 - Exemplo de um arquivo excel:</p>
-            <img class="img-exemplo-csv"  src="../../../images/excel-example.png" alt="">
-        </div>
+        <p >Obervação 02 - Exemplo de um arquivo excel:</p>
+        <img class="img-exemplo-csv"  src="../../../images/excel-example.png" alt="">
 
         <br>
-    </div>
-
-    <form method="POST" action="{{ url('import') }}" enctype="multipart/form-data">
-        @csrf   <!--Metodo de segurança para envio de forms-->
-        <div class="form-group">
-            <label for="import-form">File</label>
-            <input
-                type="file"
-                id="import-form"
-                name="import-form"
-                autocomplete="import-form"
-                class="form-control
+        <br>
+        <form method="POST" action="{{ url('import') }}" enctype="multipart/form-data">
+            @csrf   <!--Metodo de segurança para envio de forms-->
+            <div class="form-group">
+                <label for="import-form">File</label>
+                <input
+                    type="file"
+                    id="import-form"
+                    name="import-form"
+                    autocomplete="import-form"
+                    class="form-control  pt-1
             @error('import-form') is-invalid @enderror"
-                value= "{{old('import-form')}}"
-                required>
-            @error('import-form')
-            <span class="invalid-feedback" role="alert">
+                    value= "{{old('import-form')}}"
+                    required>
+                @error('import-form')
+                <span class="invalid-feedback" role="alert">
             <strong>{{$message}} </strong>
         </span>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-primary mb-2">Importar</button>
-        <a type="button"  href="{{ URL::previous() }}" id="back-btn" class="btn mb-2 ">Cancelar</a>
-    </form>
-    <br>
+                @enderror
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary mb-2">Importar</button>
+            <a type="button"  href="{{ URL::previous() }}" id="back-btn" class="btn mb-2 ">Cancelar</a>
+        </form>
+
+    </div>
+
 </div>
-<br>
+
